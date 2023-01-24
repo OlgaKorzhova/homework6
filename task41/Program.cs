db5208eb38c972ce3fb34c3ef137de966432f1d4
Olga <muanaire@gmail.com>
2023-01-24 19:27:16 +0000

    Задача 41

---
+++ task41/Program.cs
@@ -0,0 +1,49 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+int [] generateArray(int length, int start, int end)
+{
+    int [] result = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);
+    }
+    return result;
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] );
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+int getCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+int number = getUserData("Введите количество чисел");
+int [] array = generateArray(number, -100, 100);
+printArray(array);
+int getCountNumbers = getCount (array);
+Console.WriteLine($"Колиество четных чисел {getCountNumbers}");
